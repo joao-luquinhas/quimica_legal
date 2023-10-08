faa2025fb0ce43008083167489129c340b63628e
joao-luquinhas <joao.anjos19@etec.sp.gov.br>
2023-10-08 19:13:53 -0300

    projeto atualizado

---
+++ Projeto quimica legal/js/form.js
@@ -0,0 +1,48 @@
+const titulo = document.getElementById("titulo");
+const simbolo_g = document.getElementById("simbolo_g");
+const num_at_result = document.getElementById("num_at_result");
+const simbolo_result = document.getElementById("simbolo_result");
+const mas_p = document.getElementById("mas_p");
+const config_result = document.getElementById("config_result");
+
+function H(){
+	titulo.textContent = "Hidrogênio";
+	simbolo_g.textContent = "H";
+	simbolo_result.textContent = "H";
+	num_at_result.textContent ="1";
+	mas_p.textContent = "1,00784u";
+	config_result.textContent = "1s¹";
+	titulo.style.backgroundColor = "#8BC34A";
+	simbolo_g.style.backgroundColor = "#8BC34A";
+}
+
+function He(){
+	titulo.textContent = "Hélio";
+	simbolo_g.textContent = "He";
+	simbolo_result.textContent = "He";
+	num_at_result.textContent ="2";
+	mas_p.textContent = "4,002602u";
+	config_result.textContent = "1s²";
+	titulo.style.backgroundColor = "#1E90FF";
+	simbolo_g.style.backgroundColor = "#1E90FF";
+}
+
+function Li(){
+	titulo.textContent = "Lítio";
+	simbolo_g.textContent = "Li";
+	simbolo_result.textContent = "Li";
+	num_at_result.textContent ="3";
+	mas_p.textContent = "6,941u";
+	config_result.textContent = "1s²2s¹";
+	titulo.style.backgroundColor = "#FFA500";
+	simbolo_g.style.backgroundColor = "#FFA500";
+}
+  
+
+
+
+	
+	
+	
+
+	
